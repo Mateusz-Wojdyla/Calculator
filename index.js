328f4c5444bd099273827cac5cbd4d409933ab82
Mateusz Wojdyła <152260523+Mateusz-Wojdyla@users.noreply.github.com>
2024-01-30 14:10:01 +0100

    testty

--- index.js
+++ index.js
@@ -163,3 +163,152 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
+
+
+
+
+
+
+
+
+
+
+// let srt = "Hello World";
+// console.log(srt);
+
+// let srt2 = srt + " and hello again!";
+// console.log(srt2);
+// console.log(srt2.length);
+
+// console.log(srt2[0]);
+// console.log(srt2[1]);
+// console.log(srt2[7]);
+// console.log(srt2[ srt2.length - 1]);
+
+// let text = "acccążłę";
+
+
+// let a = "Gdyby kuzka nie skakała toby nózki nie złamała"
+// console.log(a);
+// console.log(a[0]);
+// console.log(a[a.length - 1])
+// let txt = "🐐";
+
+// let result = a + txt;
+// console.log(result);
+
+// let str = "raz dwa trzy";
+
+// let animal = "psa";
+// let txt = `ola ma kota,
+// natomiast kasia ma ${animal}`;
+// console.log(txt);
+
+
+// let city = "Madrycie"
+// let txt = `życie jak w ${city}`
+// console.log(txt);
+
+// let msgSent = false;
+//  msgSent = true;
+
+//  let b1 = 14 > 7;
+//  console.log(b1)
+
+//  let b2 = 12 > 15;
+//  console.log(b2)
+
+//  let b3 = 10 == 10;
+//  console.log(b3);
+
+//  let b4 = 10 != 21;
+//  console.log(b4)
+
+//  let b5 = 5 != 5;
+//  console.log(b5)
+
+// let b1 = 213123123123123123123123123123123n;
+// let result = b1 + 123123123n;
+// console.log(result)
+
+// let data;
+// console.log(data);
+// data = 10;
+
+
+// let currentTask = "task #1";
+// currentTask = null;
+
+// currentTask = "Task #2"
+
+// let txt = "Kasia";
+// let num = 23;
+// let bool = true;
+// let bigInt =121312312312312312n;
+// let data; // undefined
+
+// txt = null; // pusta wartość 
+
+// let uniqueSymbol = symbol(); // symbol
+
+// let countryName = "Poland";
+// let countryPopulation = 30_000_000;
+
+// let country = {
+//   name: "Poland",
+//   population: 38_000_000,
+//   continent: "Europe",
+//   cities: ["warszawa", "Kraków","Poznań"],
+//   capital:{
+//     name:"Warszawa",
+//     population: 2000000
+//   }
+// };
+// // wyjmowanie poszczegulnych wartości z obiektu 
+// console.log(country);
+// console.log(country.name); // Poland 
+// console.log(country.population);// 38000000
+// console.log(country.capital.name); // Warszawa
+// console.log(country.capital.population);// 2000000
+
+// let obj = {};// pusty obiekt
+
+// let carInformation = {
+//   Brand: "Porshe",
+//   Model: 911,
+//   Engine: 3.6,
+//   maxSpeed: 280,
+//   color: "Black",
+
+// };
+// console.log(carInformation);
+
+// carInformation.Brand = "Audi";
+// console.log(carInformation);
+
+  //TABLICE
+// let brands = ["sony", "lenowo","google","dell"];
+// console.log(brands);
+// console.log(brands[0]);// Sony
+// console.log(brands[1]);//Lenowao
+// console.log(brands[2]); //Google
+// console.log(brands.length);//4
+
+// brands[3] = "acer";  //ZMIANA NAZWY 3 nazwa w tabeli na acer
+// console.log(brands);
+// let data = [1, 2, true, "Asia", a{a:12, b:15}];
+
+
+// let arrObjects = [
+//   {name:"Kasia", age: 28, favColor: "red"},
+//   {name: "Ola", age: 22, favColor: "yellow"},
+//   {name: "Adam", age: 44, favColor: "black"},
+// ];
+//   console.log(arrObjects);
+
+// const array = ["string", "number", "bigInt", "boolean", "null", "undefinded", "symbol"];
+// console.log(array);
+// console.log(array[0]);
+// console.log(array[array.length - 1]);
+// array [1] = "number!";
+// console.log(array);
